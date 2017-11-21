02e2979e564720646081d5226d36470396ac0224
scrat98 <artemgolovko98@gmail.com>
2017-11-21 22:36:08 +0300

    first commit

--- lab4_ThreadsExamples/ThreadPool.hpp
+++ lab4_ThreadsExamples/ThreadPool.hpp
@@ -1,9 +1,9 @@
 #ifndef LAB4_THREADSEXAMPLES_THREADPOOL_HPP
 #define LAB4_THREADSEXAMPLES_THREADPOOL_HPP
+// https://www.youtube.com/watch?v=LL8wkskDlbs&list=PL5jc9xFGsL8E12so1wlMS0r0hTQoJL74M see the lessons
 
 #include <future>
 #include <functional>
-#include <ctime>
 #include <thread>
 #include <chrono>
 #include <mutex>
@@ -33,32 +33,54 @@ private:
 
     std::mutex mtx;
     std::condition_variable cv;
-    bool stop;
+//  bool stop;
+
+//  thread can caches this variable. But mutable it's bad practice
+//  http://en.cppreference.com/w/cpp/atomic/atomic
+    std::atomic<bool> stop;
 };
 
 // It's bad practice
 // 1) do useless checks in infinity loop
 // 2) lock/unlock mutex for all threads. And n-1 threads will know, that mutex has locked yet
+//ThreadPool::ThreadPool(size_t threadsCount) : stop(false) {
+//    for (size_t i = 0; i < threadsCount; i++) {
+//        threads.emplace_back([this]() {
+//            while (true) {
+//                std::function<void()> task;
+//                if (!tasks.empty() || stop) {
+//                    std::unique_lock<std::mutex> lock(mtx);
+//                    if (stop && tasks.empty()) return;
+//                    else if (tasks.empty()) continue;
+//                    task = std::move(tasks.front());
+//                    tasks.pop();
+//                }
+//                if (task) task();
+//            }
+//    }
+//}
+
+//        });
+// To avoid logic above we should use condition_variable
+// http://en.cppreference.com/w/cpp/thread/condition_variable
 ThreadPool::ThreadPool(size_t threadsCount) : stop(false) {
     for (size_t i = 0; i < threadsCount; i++) {
         threads.emplace_back([this]() {
             while (true) {
                 std::function<void()> task;
-                if (!tasks.empty() || stop) {
+                /* if (!tasks.empty() || stop)*/ {
                     std::unique_lock<std::mutex> lock(mtx);
+                    cv.wait(lock, [this]() { return !tasks.empty() || stop == true; });
                     if (stop && tasks.empty()) return;
-                    else if (tasks.empty()) continue;
+                    // else if (tasks.empty()) continue;
                     task = std::move(tasks.front());
                     tasks.pop();
                 }
-                if (task) task();
+                /*if (task)*/ task();
             }
         });
     }
 }
-
-// To avoid logic above we should use condition_variable
-// http://en.cppreference.com/w/cpp/thread/condition_variable
 
 template<class F, class... Args>
 auto ThreadPool::addTask(F&& function, Args&& ... args)
@@ -66,33 +88,38 @@ auto ThreadPool::addTask(F&& function, Args&& ... args)
     using return_type = typename std::result_of<F(Args...)>::type;
 
 //  it doesn't work. see below (Fucking c++)
-    auto task = std::packaged_task<return_type()>(
-            [function, args...]() {
-                return function(args...);
-            }
-    );
-    auto result = task.get_future();
+//    auto task = std::packaged_task<return_type()>(
+//            std::bind(std::forward<F>(function), std::forward<Args>(args)...)
+//    );
+//    auto result = task.get_future();
 
 // make copy with std::make_shared, cause std::function in our queue requires copyable elements,
 // but promise isn't(it's only movable - std::move)
-//    auto task = std::make_shared<std::packaged_task<return_type()>>(
-//            [function, args...]() {
-//                return function(args...);
-//            }
-//    );
-//    auto result = task->get_future();
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(function), std::forward<Args>(args)...)
+    );
+    auto result = task->get_future();
 
     {
         std::unique_lock<std::mutex> lock(mtx);
         if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
-        tasks.emplace([task]() { task(); });
+        tasks.emplace([task]() { (*task)(); });
     }
 
+    cv.notify_one();
     return result;
 };
 
 ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        stop = true;
+    }
+    cv.notify_all();
 
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
 }
 
 #endif LAB4_THREADSEXAMPLES_THREADPOOL_HPP
--- lab4_ThreadsExamples/main.cpp
+++ lab4_ThreadsExamples/main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include <thread>
+#include <ctime>
+#include <tuple>
+#include <chrono>
 #include "ThreadPool.hpp"
 
-void foo() {
-    std::cout << "Task 1";
-}
 int main() {
+    srand(time(NULL));
     std::cout << std::thread::hardware_concurrency() << " concurrent threads are supported." << std::endl;
-    ThreadPool threadPool(8);
-//    threadPool.addTask([](){std::cout << "Task 1";});
-    threadPool.addTask(foo);
+
+    ThreadPool threadPool(4);
+    std::vector<std::future<std::pair<int, int>>> results;
+
+    for (int i = 0; i < 1e6; ++i) {
+        results.emplace_back(
+                threadPool.addTask([](int i, int rand) {
+                    int x = (i + rand) * rand;
+                    return std::make_pair(i, x);
+                }, i, rand())
+        );
+    }
+
+    for (auto& result : results) {
+        auto res = result.get();
+        std::cout << "Task = " << res.first
+                  << " Number = " << res.second << std::endl;
+    }
+    std::cout << std::endl;
 
     system("pause");
 
